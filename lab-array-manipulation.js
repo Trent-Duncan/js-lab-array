59d3b01ca27b402805ac0c27c4ef90d534c3000b
Trent-Duncan <duncantr131@northwested.org>
2026-04-07 10:25:07 -0400

    Update lab-array-manipulation.js

--- lab-array-manipulation.js
+++ lab-array-manipulation.js
@@ -3,5 +3,5 @@
 // Array Methods Practice
 
 const heroes = ["Iron-Man", "The Incredible Hulk", "Thor God of Thunder", "Spider-Man","Vison"];
-alert(`Initial superhero team size: ${heros.length}`);
+alert(`Initial superhero team size: ${heroes.length}`);
 console.log(`Current superhero team: ${heroes}`);
