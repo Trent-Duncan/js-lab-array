a24b5dbb98e736bb472ce6ac9f794a8e20c9d30a
Trent-Duncan <duncantr131@northwested.org>
2026-04-07 10:18:30 -0400

    Update lab-array-manipulation.js

--- lab-array-manipulation.js
+++ lab-array-manipulation.js
@@ -1,1 +1,7 @@
+// Trent Duncan
+// 4/7/2026
+// Array Methods Practice
 
+const heroes = ["Iron-Man", "The Incredible Hulk", "Thor God of Thunder", "Spider-Man","Vison"];
+alert(`Initial superhero team size: ${heros.length}`);
+console.log(`Current superhero team: ${heroes}`);
